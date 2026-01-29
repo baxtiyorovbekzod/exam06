ff9822ee69ede4ffd24990da8c608fb7facbafca
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2026-01-29 23:46:04 +0500

    ok

--- core/settings.py
+++ core/settings.py
@@ -117,10 +117,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# project root static papka ishlatiladi
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # project root static papkasi
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
